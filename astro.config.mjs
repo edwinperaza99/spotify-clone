fbd0e08cd48e8a541c7a3ece28a3aa0eefe1a81b
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-09-09 19:20:19 -0700

    fix Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: vercel(),
-	integrations: [
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		react(),
+  integrations: [
+      tailwind({
+          applyBaseStyles: false,
+      }),
+      react(),
 	],
-	output: "hybrid",
+
+  output: "hybrid",
+  adapter: vercel(),
 });
