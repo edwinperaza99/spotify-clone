ee6e3d5d81de07617ea1402993a1cc492460d39d
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-09-09 19:19:28 -0700

    solve Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server',
+    adapter: vercel(),
 	integrations: [
 		tailwind({
 			applyBaseStyles: false,
